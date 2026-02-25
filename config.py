b6337114d537de0d91278dce580f147d458e13ea
Mouryaveer <mouryaveerast@gmail.com>
2026-02-25 18:24:44 +0530

    Update config for Render deployment

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a9edf4b2bd26269eb78aa19a5c635626'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
